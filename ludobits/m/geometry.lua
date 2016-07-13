f93c58668b95f004e0f07738033b26405283f4fe
Björn Ritzl <bjorn.ritzl@king.com>
2016-07-13 09:51:29 +0200

    Added geometry distance

--- ludobits/m/geometry.lua
+++ ludobits/m/geometry.lua
@@ -9,10 +9,13 @@ function M.to_radians(degrees)
 	return degrees * math.pi / 180
 end
 
-function M.angle_towards(from_position, to_position)
-	local angle = math.atan2(to_position.x - from_position.x, from_position.y - to_position.y)
+function M.angle_towards(from, to)
+	local angle = math.atan2(to.x - from.x, from.y - to.y)
 	return vmath.quat_axis_angle(vmath.vector3(0, 0, 1), angle)
 end
 
+function M.distance(from, to)
+	return math.sqrt(math.abs(from.x - to.x) ^ 2 + math.abs(from.y - to.y) ^ 2)
+end
 
 return M
