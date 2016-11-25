f9a42481801e0056072537ef2ea6a2be81dfbb1c
Björn Ritzl <bjorn.ritzl@king.com>
2016-11-25 07:36:19 +0100

    The dynamic module will now use the mass of the collision object when applying forces

--- ludobits/m/dynamic.lua
+++ ludobits/m/dynamic.lua
@@ -2,15 +2,17 @@ local M = {}
 
 local LINEAR_VELOCITY = hash("linear_velocity")
 local ANGULAR_VELOCITY = hash("angular_velocity")
+local MASS = hash("mass")
 
 --- Rotate a collision object be applying opposing and offset forces
 -- @param collisionobject_url
 -- @param force In the format of vmath.vector3(0, force, 0)
 function M.rotate(collisionobject_url, force)
+	local mass = go.get(collisionobject_url, MASS)
 	local rotation = go.get_rotation()
 	local world_position = go.get_world_position()
-	msg.post(collisionobject_url, "apply_force", { force = vmath.rotate(rotation, force), position = world_position + vmath.rotate(rotation, vmath.vector3(-50, 50, 0)) })
-	msg.post(collisionobject_url, "apply_force", { force = vmath.rotate(rotation, -force), position = world_position + vmath.rotate(rotation, vmath.vector3(50, -50, 0)) })
+	msg.post(collisionobject_url, "apply_force", { force = vmath.rotate(rotation, force * mass), position = world_position + vmath.rotate(rotation, vmath.vector3(-50, 50, 0)) })
+	msg.post(collisionobject_url, "apply_force", { force = vmath.rotate(rotation, -force * mass), position = world_position + vmath.rotate(rotation, vmath.vector3(50, -50, 0)) })
 end
 
 
@@ -19,7 +21,8 @@ end
 -- @param collisionobject_url
 -- @param force In the format of vmath.vector3(0, force, 0)
 function M.forward(collisionobject_url, force)
-	msg.post(collisionobject_url, "apply_force", { force = vmath.rotate(go.get_rotation(), force), position = go.get_world_position() })
+	local mass = go.get(collisionobject_url, MASS)
+	msg.post(collisionobject_url, "apply_force", { force = vmath.rotate(go.get_rotation(), force * mass), position = go.get_world_position() })
 end
 
 --- Move a dynamic collision object in the opposite direction of
@@ -27,11 +30,14 @@ end
 -- @param collisionobject_url
 -- @param force In the format of vmath.vector3(0, force, 0)
 function M.backwards(collisionobject_url, force)
-	msg.post(collisionobject_url, "apply_force", { force = vmath.rotate(go.get_rotation(), -force), position = go.get_world_position() })
+	local mass = go.get(collisionobject_url, MASS)
+	msg.post(collisionobject_url, "apply_force", { force = vmath.rotate(go.get_rotation(), -force * mass), position = go.get_world_position() })
 end
 
 function M.stop_moving(collisionobject_url)
-	msg.post(collisionobject_url, "apply_force", { force = -go.get(collisionobject_url, LINEAR_VELOCITY) * 10, position = go.get_world_position() })
+	local mass = go.get(collisionobject_url, MASS)
+	local linv = go.get(collisionobject_url, LINEAR_VELOCITY)
+	msg.post(collisionobject_url, "apply_force", { force = -linv * 100 * mass, position = go.get_world_position() })
 end
 
 function M.stop_rotating(collisionobject_url)
