4c0b536477a478fbe5ac032715eee838a3a087ca
Björn Ritzl <bjorn.ritzl@king.com>
2017-06-12 09:06:37 +0200

    sys.save is nowadays atomic
    No need to handle this ourselves

--- ludobits/m/savefile.lua
+++ ludobits/m/savefile.lua
@@ -10,9 +10,7 @@
 -- file.save({ foo = "bar" })
 --
 
-local M = {
-	tmpname = "___lbtmp"
-}
+local M = {}
 
 function M.get_path(filename)
 	local path = sys.get_save_file(sys.get_config("project.title"):gsub(" ", "_"), filename)
@@ -38,20 +36,11 @@ function M.open(filename)
 	-- @return error_message
 	function instance.save(t)
 		assert(t and type(t) == "table", "You must provide a table to save")
-		--local tmpname = ((sys.get_sys_info().system_name == "Windows") and os.getenv("TMP") or "") .. os.tmpname()
-		-- os.tmpname() is not very reliable on windows (it doesn't return a valid path to a temp file)
-		-- it's better to return the path to an application local file, and in this case we use the same filename
-		-- every time so that we do not pollute the filesystem with many temporary files in case of problems
-		-- when writing to the file
-		-- it is reasonable to assume that a filename such as "___lbtmp" isn't used by any other part of the
-		-- system, and if that is actually the case it is possible to change it
-		local tmpname = M.get_path(M.tmpname)
-		local success = sys.save(tmpname, t)
+		local success = sys.save(path, t)
 		if not success then
 			return false, "Unable to save file"
 		end
-		os.remove(path)
-		return os.rename(tmpname, path)
+		return true
 	end
 	
 	return instance
