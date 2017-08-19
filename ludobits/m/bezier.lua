81450bdf999f32dba8e2e6cda4ca85b4f7b656fc
Björn Ritzl <bjorn.ritzl@king.com>
2017-08-19 12:09:48 +0200

    Added bezier

---
+++ ludobits/m/bezier.lua
@@ -0,0 +1,43 @@
+local M = {}
+
+
+local function point_on_cubic_bezier(cp, t)
+	local ax, bx, cx
+	local ay, by, cy
+	local tSquared, tCubed
+	
+	--  /* calculation of the polinomial coeficients */
+	
+	cx = 3.0 * (cp[2].x - cp[1].x)
+	bx = 3.0 * (cp[1].x - cp[2].x) - cx
+	ax = cp[4].x - cp[1].x - cx - bx
+	
+	cy = 3.0 * (cp[2].y - cp[1].y)
+	by = 3.0 * (cp[3].y - cp[2].y) - cy
+	ay = cp[4].y - cp[1].y - cy - by
+	
+	--  /* calculate the curve point at parameter value t */
+	
+	tSquared = t * t
+	tCubed = tSquared * t
+	
+	local result = vmath.vector3()
+	result.x = (ax * tCubed) + (bx * tSquared) + (cx * t) + cp[1].x
+	result.y = (ay * tCubed) + (by * tSquared) + (cy * t) + cp[1].y
+	return result
+end
+
+
+function M.create(cp, points)
+	local curve = {}
+	local dt = 1.0 / (points - 1)
+	local i
+
+	for i = 1, points do
+		curve[i] = point_on_cubic_bezier(cp, i * dt)
+		i = i + 1
+	end
+	return curve
+end
+
+return M
