f8b5cbff95dc3bd89cf8a577a99a4881fe24f6c5
Björn Ritzl <bjorn.ritzl@king.com>
2017-09-19 10:56:38 +0200

    Added signal.lua

---
+++ ludobits/m/signal.lua
@@ -0,0 +1,39 @@
+local M = {}
+
+function M.create(signal_id)
+	assert(signal_id, "You must provide a signal_id")
+	local signal = {}
+
+	local listeners = {}
+
+	function signal.add(cb)
+		assert(cb, "You must provide a callback")
+		if type(cb) == "function" then
+			listeners[cb] = { fn = cb }
+		else
+			local key = hash_to_hex(cb.socket or hash("")) .. hash_to_hex(cb.path or hash("")) .. hash_to_hex(cb.fragment or hash(""))
+			listeners[key] = { fn = function(message) msg.post(cb, signal_id, message) end }
+		end
+	end
+
+	function signal.remove(cb)
+		assert(cb, "You must provide a callback")
+		if type(cb) == "function" then
+			listeners[cb] = nil
+		else
+			local key = hash_to_hex(cb.socket or hash("")) .. hash_to_hex(cb.path or hash("")) .. hash_to_hex(cb.fragment or hash(""))
+			listeners[key] = nil
+		end
+	end
+
+	function signal.trigger(message)
+		assert(message, "You must provide a message")
+		for k,v in pairs(listeners) do
+			v.fn(message)
+		end
+	end
+
+	return signal
+end
+
+return M
