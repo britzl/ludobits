7a9a6d983a5703e2c302aad56ca41fc6a9d6f799
Björn Ritzl <bjorn.ritzl@gmail.com>
2017-09-27 16:24:57 +0200

    Added file tests

--- ludobits/m/file.lua
+++ ludobits/m/file.lua
@@ -3,8 +3,7 @@
 local M = {}
 
 function M.fix(filename)
-	filename = filename:gsub("\n", "\r\n")
-	filename = filename:gsub("([^0-9a-zA-Z ])", function(c) string.format("%%%02X", string.byte(c)) end)
+	filename = filename:gsub("([^0-9a-zA-Z%._ ])", function(c) return string.format("%%%02X", string.byte(c)) end)
 	filename = filename:gsub(" ", "+")
 	return filename
 end
---
+++ test/test_file.lua
@@ -0,0 +1,31 @@
+local mock = require "deftest.mock"
+local mock_fs = require "deftest.mock.fs"
+
+return function()
+	local savefile = require "ludobits.m.savefile"
+	
+	describe("savefile", function()
+		before(function()
+			mock_fs.mock()
+		end)
+
+		after(function()
+			mock_fs.unmock()
+		end)
+
+		it("should be able to save and load files", function()
+			local file1 = savefile.open("foobar1")
+			local file2 = savefile.open("foobar2")
+			local t1 = file1.load()
+			local t2 = file2.load()
+			assert(not t1)
+			assert(not t2)
+			file1.save("foobar")
+
+			local t1 = file1.load()
+			local t2 = file2.load()
+			assert(t1 == "foobar")
+			assert(not t2)
+		end)
+	end)
+end
